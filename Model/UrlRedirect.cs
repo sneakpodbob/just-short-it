61f4359699ad5279c355f37f14ee2730d09bc8cb
Robert Krüger <robert.krueger@giz.de>
2023-10-09 22:30:34 +0200

    Generate shorter IDs if possible (longer if needed)

--- Model/UrlRedirect.cs
+++ Model/UrlRedirect.cs
@@ -4,4 +4,4 @@ using Microsoft.AspNetCore.Mvc;
 namespace JustShortIt.Model; 
 
 [BindProperties]
-public record UrlRedirect([Required, MinLength(2), MaxLength(16)]string Id, [Required, Url]string Target, [Required]string ExpirationDate);
+public record UrlRedirect([Required, MinLength(1), MaxLength(16)]string Id, [Required, Url]string Target, [Required]string ExpirationDate);
