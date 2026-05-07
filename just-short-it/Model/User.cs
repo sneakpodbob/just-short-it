b3c4c2fc4fb5587bc9f69e32f61bd2dda511575d
Robert Krüger <4972863+sneakpodbob@users.noreply.github.com>
2026-05-07 17:13:06 +0200

    sort comment

--- just-short-it/Model/User.cs
+++ just-short-it/Model/User.cs
@@ -1,12 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
 
-namespace JustShortIt.Model; 
+namespace JustShortIt.Model;
 
-[BindProperties]
 /// <summary>
 /// Credential model used for login binding and configuration-backed authentication.
 /// </summary>
 /// <param name="Username">Account username.</param>
 /// <param name="Password">Account password in plain text as submitted by the login form.</param>
+/// [BindProperties]
 public record User([Required]string Username, [Required]string Password);
